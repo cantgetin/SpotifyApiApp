9e3fc0dbfcaac9db3915c6dd563d02af56a44755
cantgetin <ictwyt@mail.ru>
2021-10-21 10:17:37 +0500

    implemented bottom navigation bar and new fragments

--- app/src/main/java/com/example/coursespotifyapiproject/AnalyticsFragment.kt
+++ app/src/main/java/com/example/coursespotifyapiproject/AnalyticsFragment.kt
@@ -5,11 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.coursespotifyapiproject.data.api.ApiHelper
-import com.example.coursespotifyapiproject.data.api.RetrofitBuilder
-import com.example.spotifysigninexample.SpotifyConstants
 
-class ListFragment() : Fragment() {
+class AnalyticsFragment() : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,7 +14,6 @@ class ListFragment() : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        return inflater.inflate(R.layout.list_fragment, container, false)
+        return inflater.inflate(R.layout.analytics_fragment, container, false)
     }
-
 }
--- app/src/main/java/com/example/coursespotifyapiproject/MainActivity.kt
+++ app/src/main/java/com/example/coursespotifyapiproject/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.coursespotifyapiproject
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.main_activity.*
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     val itemClickListener: () -> Unit = { ->
 
         var fragment2 = MainFragment()
+
+        navigation_bar.visibility = View.VISIBLE
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, fragment2)
@@ -22,6 +28,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
+        var bottomNav: BottomNavigationView = findViewById(R.id.navigation_bar)
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
+
+        navigation_bar.visibility = View.GONE
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, fragment1)
@@ -29,10 +40,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val navListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.navigation_user -> selectedFragment = MainFragment()
+                R.id.navigation_list -> selectedFragment = ListFragment()
+                R.id.navigation_analytics -> selectedFragment = AnalyticsFragment()
+            }
+            if (selectedFragment != null) {
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.container,
+                    selectedFragment
+                ).commit()
+            }
+            true
+        }
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         fragment1.onActivityResult(requestCode, resultCode, data)
+
     }
+
 
 }
 
